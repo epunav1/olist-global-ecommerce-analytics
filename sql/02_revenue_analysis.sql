c57e3c890cf6df09992f38df85bbbfc159c2af3d
Victor Ebubechukwu Epuna <144723497+epunav1@users.noreply.github.com>
2025-11-25 17:25:43 -0600

    02_revenue_analysis.sql

--- sql/02_revenue_analysis.sql
+++ sql/02_revenue_analysis.sql
@@ -1,1 +1,9 @@
-
+-- Monthly revenue (sum of payment values per month)
+SELECT
+    DATE_FORMAT(o.order_purchase_timestamp, '%Y-%m') AS year_month,
+    SUM(p.payment_value) AS total_revenue
+FROM olist_orders_dataset o
+JOIN olist_order_payments_dataset p
+    ON o.order_id = p.order_id
+GROUP BY DATE_FORMAT(o.order_purchase_timestamp, '%Y-%m')
+ORDER BY year_month;
